2aa1345cc34fc308b041350571028d7226538a0f
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2025-11-26 18:27:27 +0600

    Import FormEvent in login page
    Added FormEvent to the imports from react in page.tsx to support form event typing.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState,FormEvent } from "react";
 import { toast } from "sonner";
 import axios from "axios";
 import { useRouter } from "next/navigation";
